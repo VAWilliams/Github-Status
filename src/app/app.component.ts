06aa072d1a8601acbab677f11b5f242c0227a828
Vicario.Williams <Vicario.Williams@hungerrush.com>
2022-05-18 06:13:09 +0200

    updated styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { LoaderService } from './shared/services/loader.service';
 
 @Component({
@@ -9,13 +11,25 @@ import { LoaderService } from './shared/services/loader.service';
 })
 export class AppComponent implements OnInit {
 
-  title: string = 'Github Lite';
+  title: string = 'Github Status';
   isLoading: Observable<boolean>;
+  currentUrl: Observable<string>;
 
-  constructor(private loaderService: LoaderService) { }
+  constructor(
+    private loaderService: LoaderService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.isLoading = this.loaderService.getState();
+    this.currentUrl = this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(event => {
+          const navigationEnd = event as NavigationEnd;
+          return navigationEnd.url;
+        })
+      );
   }
 
 }
